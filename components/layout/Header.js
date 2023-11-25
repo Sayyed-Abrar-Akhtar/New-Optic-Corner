9fd64378a3fcbc418963860d08da49e14dc01f9f
Sayyed-Abrar-Akhtar <58516708+Sayyed-Abrar-Akhtar@users.noreply.github.com>
2023-11-25 15:48:36 +0545

    Update Header.js

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -14,7 +14,7 @@ const Header = ({ cartItems }) => {
           <Link href='/' passHref>
             <a className={styles.logo__link}>
               <Image
-                src='/logo.svg'
+                src='/logo.png'
                 alt='New Optic Corner'
                 width={600}
                 height={400}
